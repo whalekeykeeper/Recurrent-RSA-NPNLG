d1389bd0ccbff4e1c95f29e0d7b2714abf286437
waron97 <winkler.aron5gmail.com>
2023-03-12 11:01:14 +0100

    add script to download VG files

--- evaluation/build_test_sets/load_vg_dataset.py
+++ evaluation/build_test_sets/load_vg_dataset.py
@@ -1,9 +1,43 @@
 import json
 from .vg_types import Metadata, Objects, Regions
+import os
+import zipfile
+from shutil import copyfileobj
+import requests
+
+
+def _download_asset(url: str, outpath: str):
+    print("Downloading", url, "to", outpath)
+    final_zip_path = outpath + ".zip"
+    with open(final_zip_path, "wb") as f:
+        r = requests.get(url, stream=True)
+        copyfileobj(r.raw, f)
+    with zipfile.ZipFile(final_zip_path, "r") as zip_ref:
+        zip_ref.extractall(outpath)
+
+    for f in os.listdir(outpath):
+        current_path = os.path.join(outpath, f)
+        os.rename(current_path, outpath + ".json")
+
+    os.remove(final_zip_path)
+    os.rmdir(outpath)
 
 
 def download_vg_assets():
-    pass
+    objects_url = "http://visualgenome.org/static/data/dataset/objects.json.zip"
+    regions_url = "http://visualgenome.org/static/data/dataset/region_descriptions.json.zip"
+    metadata_url = "http://visualgenome.org/static/data/dataset/image_data.json.zip"
+    data_dir = os.path.join(os.getcwd(), "data", "vg")
+
+    try:
+        os.makedirs(data_dir)
+    except FileExistsError:
+        pass
+
+    _download_asset(objects_url, os.path.join(data_dir, "objects"))
+    _download_asset(regions_url, os.path.join(
+        data_dir, "region_descriptions"))
+    _download_asset(metadata_url, os.path.join(data_dir, "image_data"))
 
 
 def _load_regions() -> Regions:
