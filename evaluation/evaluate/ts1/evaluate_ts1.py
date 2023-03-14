5aaab0711cf4d0cc95ec6d36e5373fc660a7e17b
waron97 <winkler.aron5gmail.com>
2023-03-14 22:40:09 +0100

    improve logging during evaluation

--- evaluation/evaluate/ts1/evaluate_ts1.py
+++ evaluation/evaluate/ts1/evaluate_ts1.py
@@ -1,19 +1,17 @@
 from ...build_test_sets import types
 from ..helpers.evaluate_cluster import evaluate_cluster
+from tqdm import tqdm
 
 
 def evaluate_ts1(test_set: types.TS1):
     correctly_predicted = 0
     total = 0
-    for cluster in test_set:
+    for cluster in tqdm(test_set):
         is_correct_prediction = evaluate_cluster(cluster)
         if (is_correct_prediction):
             correctly_predicted += 1
         total += 1
-        print(
-            f"Evaluated TS1 / {cluster['object_type']} / cluster {cluster['cluster_id']}")
-        print(
-            f"Accuracy so far: {correctly_predicted / total} ({correctly_predicted} / {total})")
-        print("\n-----------------------\n")
+
+    print("Accuracy on TS1", correctly_predicted / total)
 
     return correctly_predicted, total
--- evaluation/evaluate/ts2/evaluate_ts2.py
+++ evaluation/evaluate/ts2/evaluate_ts2.py
@@ -1,17 +1,15 @@
 from ...build_test_sets import types
 from ..helpers.evaluate_cluster import evaluate_cluster
+from tqdm import tqdm
 
 
 def evaluate_ts2(test_set: types.TS2):
     correctly_predicted = 0
     total = 0
-    for cluster in test_set:
+    for cluster in tqdm(test_set):
         is_correct_prediction = evaluate_cluster(cluster)
         if (is_correct_prediction):
             correctly_predicted += 1
         total += 1
-        print(
-            f"Evaluated TS2 / cluster {cluster['cluster_id']}")
-        print(
-            f"Accuracy so far: {correctly_predicted / total} ({correctly_predicted} / {total})")
-        print("\n-----------------------\n")
+
+    print("Accuracy on TS2", correctly_predicted / total)
