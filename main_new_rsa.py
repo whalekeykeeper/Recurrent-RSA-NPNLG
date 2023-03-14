6ca515e70ce873dfb6d105e586d6b7f7c5266049
waron97 <winkler.aron5gmail.com>
2023-03-14 08:34:42 +0100

    update new main script to read example images from a test set

--- main_new_rsa.py
+++ main_new_rsa.py
@@ -7,29 +7,33 @@ from keras.preprocessing import image
 
 from bayesian_agents._joint_rsa import RSA, SamplingStrategy, SpeakerType
 from utils.config import *
+import os
 
 matplotlib.use("Agg")
 
 urls = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnaWSDM__KaWGxTPrpMpwF5DvgJ-U4cfyL4g&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcrNMOpyvmHPShc9fb6tLar5UJnReaNmj7bQ&usqp=CAU",
 ]
 
+data_dir = "data/test_sets/ts1/man/3"
+for ip in os.listdir(data_dir):
+    urls.append(os.path.join(data_dir, ip))
+
+print(urls)
 
 # the model starts of assuming it's equally likely any image is the intended referent
-model = RSA(images=urls)
+model = RSA(images=urls, urls_are_local=True)
 
 GREEDY_caption = model.sample(
     strategy=SamplingStrategy.GREEDY,
     speaker_type=SpeakerType.LITERAL,
-    target_image_idx=1,
+    target_image_idx=0,
     speaker_rationality=5
 )
 
 BEAM_caption = model.sample(
     strategy=SamplingStrategy.BEAM,
     speaker_type=SpeakerType.PRAGMATIC,
-    target_image_idx=1,
+    target_image_idx=0,
     speaker_rationality=5,
     n_beams=10,
     cut_rate=1
@@ -37,3 +41,8 @@ BEAM_caption = model.sample(
 
 print("GREEDY caption:\n", GREEDY_caption)
 print("BEAM caption:\n", BEAM_caption)
+
+print("------")
+print(model.compute_posterior(GREEDY_caption[0]))
+print("------")
+print(model.compute_posterior(BEAM_caption[0]))
